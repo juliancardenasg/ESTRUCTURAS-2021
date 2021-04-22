a45365825c6d923da17117b41bd523f170134f97
Ang-m4 <71788224+Ang-m4@users.noreply.github.com>
2021-04-21 20:31:05 -0500

    Primera Subida
    Se subió la primera versión del proyecto

---
+++ Proyecto Estructuras/funciones.hxx
@@ -0,0 +1,215 @@
+#include "funciones.h"
+#include <iostream>
+#include <cstring>
+#include <vector>
+#include <string.h>
+#include <cstring>
+#include <fstream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<string> cargar(string archivo)
+{
+    stringstream auxiliar;
+    string linea;
+    ifstream entrada;
+    vector<string> destino;
+    int cont = 0;
+
+    entrada.open(archivo);    
+
+    while (!entrada.eof())
+    {
+        getline(entrada, linea);
+
+        if (linea.find(">") != std::string::npos && cont != 0)
+        {
+            destino.push_back(auxiliar.str());
+            auxiliar.str(std::string());
+        }
+        auxiliar << linea + "\n";
+        cont++;
+    }
+
+    destino.push_back(auxiliar.str());
+    entrada.close();
+    return destino;
+}
+
+vector<string> enmascarar(vector<string> secuencias, string mascara, int *total)
+{
+
+    char *aux = new char[mascara.length()];
+    int cont = 0;
+
+    for (int j = 0; j < mascara.length(); j++)
+    {
+        aux[j] = 'X';
+    }
+
+    for (int i = 0; i < secuencias.size(); i++)
+    {
+        if (secuencias[i].find(mascara) <= secuencias[i].length())
+        {
+            cont++;
+        }
+
+        while (secuencias[i].find(mascara) <= secuencias[i].length())
+        {
+            secuencias[i].replace(secuencias[i].find(mascara), mascara.length(), aux);
+        }
+    }
+    *total = cont;
+    return secuencias;
+}
+
+vector<string> es_subsecuencia(vector<string> secuencias, string secuencia, int *numSec)
+{
+
+    int cont = 0;
+
+    for (int i = 0; i < secuencias.size(); i++)
+    {
+        if (secuencias[i].find(secuencia) <= secuencias[i].length())
+        {
+            cont++;
+        }
+    }
+
+    *numSec = cont;
+    return secuencias;
+}
+
+void listar_secuencia(vector<string> secuencias)
+{
+    int cont = 0;
+    char *aux;
+    bool completa;
+
+    for (int i = 0; i < secuencias.size(); i++)
+    {
+        completa = true;
+        cont = 0;
+
+        if (secuencias[i].find("-") <= secuencias[i].length())
+        {
+            completa = false;
+        }
+
+        if (secuencias[i].find("A") <= secuencias[i].length())
+        {
+            cont++;
+        }
+        if (secuencias[i].find("C") <= secuencias[i].length())
+        {
+            cont++;
+        }
+        if (secuencias[i].find("G") <= secuencias[i].length())
+        {
+            cont++;
+        }
+        if (secuencias[i].find("T") <= secuencias[i].length())
+        {
+            cont++;
+        }
+        if (secuencias[i].find("U") <= secuencias[i].length())
+        {
+            cont++;
+        }
+
+        if (completa)
+        {
+            cout << "Secuencia " << secuencias[i].substr(0, secuencias[i].find("\n")) << " contiene " << cont << " bases" << endl;
+        }
+
+        else
+        {
+
+            cout << "Secuencia " << secuencias[i].substr(0, secuencias[i].find("\n")) << " contiene al menos " << cont << " bases" << endl;
+        }
+    }
+}
+
+void historgrama(vector<string> secuencias, string secuencia)
+{
+
+    char codigos[18] = {'A', 'C', 'G', 'T', 'U', 'R', 'Y', 'K', 'M', 'S', 'W', 'B', 'D', 'H', 'V', 'N', 'X', '-'};
+    int cont = 0;
+
+    bool existe = false;
+
+    for (int i = 0; i < secuencias.size(); i++)
+    {
+        if (secuencias[i].find(secuencia) <= secuencias[i].length())
+        {
+            existe = true;
+
+            for (int k = 0; k < 18; k++)
+            {
+                cont = 0;
+
+                for (int j = 0; j < secuencias[i].length(); j++)
+                {
+
+                    if (secuencias[i].at(j) == codigos[k])
+                    {
+                        cont++;
+                    }
+                }
+
+                cout << codigos[k] << " : " << cont << endl;
+            }
+        }
+    }
+
+    if (existe == false)
+    {
+        cout << "Secuencia invalida " << endl;
+    }
+
+}
+
+int guardar(vector<string> secuencias, string destino)
+{
+
+    ofstream salida;
+    salida.open(destino);
+
+    if (salida.is_open())
+    {
+
+        for (int i = 0; i < secuencias.size(); i++)
+        {
+
+            salida << secuencias[i];
+        }
+
+        salida.close();
+        return 0;
+    }
+
+    return -1;
+}
+
+void imprimirAyuda(){
+
+    cout<<"\nMENU DE AYUDA:"<<endl;
+    cout<<"**cargar (-nombre del archivo)"<<endl;
+    cout<<"  Para cargar un archivo ya existente ingrese\n  el comando como se indica en pantalla seguido\n  del nombre del archivo.\n"<<endl;
+    cout<<"**conteo"<<endl;
+    cout<<"  El comando conteo no posee argumentos, se usa\n  con el fin de contar las secuencias cargadas\n  en el sistema.\n"<<endl;
+    cout<<"**listar_secuencias"<<endl;
+    cout<<"  El comando listar_secuencias no posee argumentos\n  se usa con el fin de listar las bases que posee\n  cada secuencia y se escribe como se indica en\n  pantalla.\n"<<endl;
+    cout<<"**Histograma (-descripcion de la secuencia)"<<endl;
+    cout<<"  El comando Histograma recibe como parametro la\n  descripcion de una secuencia (se recomienda utilizar\n  listar_secuencias previamente), mostrara un conteo\n  de cada codigo presente en la secuencia seleccionada.\n"<<endl;
+    cout<<"**es_subsecuencia (-subsecuencia a buscar)"<<endl;
+    cout<<"  Este comando recibe como unico parametro una\n  subsecuencia a buscar dentro de todas las secuencias\n  cargadas, posterior a esto se indica en cuantas secuencias\n  existe la subsecuencia dada.\n"<<endl;
+    cout<<"**enmascarar (-subsecuencia a enmascarar)"<<endl;
+    cout<<"  Este comando recibe como unico parametro una\n  subsecuencia o serie de codigos a enmascarar en todas\n  las secuencias cargadas en memoria, posteriomente indicara\n  cuantas secuencias fueron enmascaradas.\n"<<endl;
+    cout<<"**guardar (-nombre del archivo)"<<endl;
+    cout<<"  El comando guardar recibe como unico parametro el\n  nombre del archivo en el que se guardaran las secuencias\n  existentes en memoria, usualmente despues de modificaciones.\n"<<endl;
+    cout<<"**salir"<<endl;
+    cout<<"  Este comando lo sacara del sistema inmediatamente."<<endl;
+
+}
---
+++ Proyecto Estructuras/main.cpp
@@ -0,0 +1,179 @@
+#include <iostream>
+#include <cstring>
+#include <vector>
+#include <string.h>
+#include <cstring>
+#include <fstream>
+#include <bits/stdc++.h>
+#include "funciones.h"
+
+using namespace std;
+
+// comando 1 - se cargan las secuencias en el vector de strings "destino"
+
+int main()
+{
+  string comando;
+  string intermedio;
+  vector<string> split;
+  vector<string> secuencias;
+  stringstream chek1;
+  int sec_enmascaradas;
+  int sec_subsecuencias;
+  cout << "--------MANIPULACION DE ARCHIVOS FASTA--------\n";
+  while (1)
+    {
+        split.clear();
+        chek1.clear();
+        cout << "$ ";
+        getline(cin, comando);
+        chek1 << comando;
+
+        while (getline(chek1, intermedio, ' '))
+        {
+            split.push_back(intermedio);
+        }
+
+        //COMANDO 1
+
+        if (split[0].compare("cargar") == 0)
+        {
+
+            secuencias = cargar((split[1]));
+
+            if (secuencias.size() == 1 && secuencias[0].find(">") == std::string::npos)
+            {
+
+                cout << split[1] << " No contiene ninguna secuencia " << endl;
+                secuencias.clear();
+            }
+            else
+            {
+
+                cout << secuencias.size() << " Secuencias cargadas desde el archivo " << split[1] << endl;
+            }
+        }
+
+        //COMANDO 2
+
+        if (split[0].compare("conteo") == 0)
+        {
+            cout << secuencias.size() << " secuencia(s) cargadas en memoria" << endl;
+        }
+
+        //COMANDO 3
+
+        if (split[0].compare("listar_secuencias") == 0)
+        {
+            if (secuencias.empty())
+            {
+                cout << "No hay secuencias cargadas en memoria." << endl;
+            }
+            else
+            {
+
+                listar_secuencia(secuencias);
+            }
+        }
+
+        //COMANDO 4
+
+        if (split[0].compare("histograma") == 0)
+        {
+
+            historgrama(secuencias, split[1]);
+        }
+
+        //COMANDO 5
+
+        if (split[0].compare("es_subsecuencia") == 0)
+        {
+            sec_subsecuencias = 0;
+
+            if (secuencias.empty())
+            {
+
+                cout << "No hay secuencias cargadas en memoria." << endl;
+            }
+            else
+            {
+                secuencias = es_subsecuencia(secuencias, split[1], &sec_subsecuencias);
+
+                if (sec_subsecuencias == 0)
+                {
+                    cout << "La secuencia no existe." << endl;
+                }
+                else
+                {
+                    cout << "se encontraron " << sec_subsecuencias << " Secuencias." << endl;
+                }
+            }
+        }
+
+        //COMANDO 6
+
+        if (split[0].compare("enmascarar") == 0)
+        {
+            sec_enmascaradas = 0;
+
+            if (secuencias.empty())
+            {
+
+                cout << "No hay secuencias cargadas en memoria." << endl;
+            }
+            else
+            {
+                secuencias = enmascarar(secuencias, split[1], &sec_enmascaradas);
+
+                if (sec_enmascaradas == 0)
+                {
+                    cout << "La secuencia no existe." << endl;
+                }
+                else
+                {
+                    cout << "se enmascararon " << sec_enmascaradas << " Secuencias." << endl;
+                }
+            }
+        }
+
+        //COMANDO 7
+
+        if (split[0].compare("guardar") == 0)
+        {
+            int validacion = guardar(secuencias, split[1]);
+            if (secuencias.empty())
+            {
+
+                cout << "No hay secuencias cargadas en memoria." << endl;
+            }
+            else
+            {
+                if (validacion != 0)
+                {
+                    cout << "Error guardando en " << split[1] << endl;
+                }
+                else
+                {
+                    cout << "Las secuencias han sido guardadas en " << split[1] << endl;
+                }
+            }
+        }
+
+        //COMANDO 8
+        if (split[0].compare("salir") == 0)
+        {
+            cout << endl;
+            exit(0);
+        }
+
+        //COMANDO AYUDA
+        if (split[0].compare("ayuda") == 0)
+        {
+            imprimirAyuda();            
+        }
+
+        cout << endl;
+    }
+
+    return 0;
+};
